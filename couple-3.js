8b4d6d3efbb6f801f0085117d6cb3251cc622459
Alex Beige <35375321+alex-beige@users.noreply.github.com>
2025-12-19 02:31:04 -0500

    small update

--- couple-3.js
+++ couple-3.js
@@ -773,7 +773,7 @@ gsap.utils
         ),
         split_heading = new SplitText(heading, { type: "words" }),
         subheading = group.querySelector(
-          "p:has(~ .spacer-heading-wrap)"
+          ".spacer-heading-wrap ~ p"
         ),
         split_subheading = new SplitText(subheading, { type: "lines,words" });
         //gridCells = group.querySelectorAll(".work_section-grid > .grid-cell:not(:first-child)");
