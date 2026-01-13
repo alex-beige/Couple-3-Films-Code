ad1583cf3c9c81fe8bc265832e413bd5a20fcfea
Alex Beige <35375321+alex-beige@users.noreply.github.com>
2026-01-12 22:45:59 -0500

    both paragraphs

--- couple-3-work.js
+++ couple-3-work.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
   elements.introElements = elements.introContainer.querySelectorAll("[gsap-intro-el]");
     // Split text into words for stagger animation
   const split_h1 = new SplitText(elements.heroText, { type: "words" }),
-  split_paragraph = new SplitText(elements.introContainer.querySelector(".rich-text p"), { type: "words,lines" });
+  split_paragraph = new SplitText(elements.introContainer.querySelectorAll(".rich-text p"), { type: "words,lines" });
   const tl_intro = gsap.timeline({});
 
   tl_intro
