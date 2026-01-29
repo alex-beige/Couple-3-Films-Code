8f524cd3603ba850cf7edea4fb7b0fb5495cf431
Alex Beige <35375321+alex-beige@users.noreply.github.com>
2026-01-29 16:59:14 -0500

    even faster

--- couple-3-work.js
+++ couple-3-work.js
@@ -120,7 +120,7 @@ ScrollTrigger.config({
 
 const carousel_lists = gsap.utils.toArray('.carousel-wrap');
 let carousel_length = document.querySelectorAll('.carousel_item-logo').length;
-const carousel_duration = carousel_length * 3.8; // adjust speed
+const carousel_duration = carousel_length * 3; // adjust speed
 
 // Set second list immediately after first
 //gsap.set(carousel_lists[1], { xPercent: 100 });
